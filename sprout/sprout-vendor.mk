8fce9289ea80d867ad502228889ef0b271b08c4e
Samar Vispute <samarvispute121@gmail.com>
2017-07-27 15:03:19 +0530

    Update sprout-vendor.mk

--- sprout/sprout-vendor.mk
+++ sprout/sprout-vendor.mk
@@ -1,15 +1,14 @@
-# Copyright (C) 2014 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/app,system/app)
 
-$(call inherit-product, vendor/google/sprout/sprout-vendor-blobs.mk)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/xbin,system/xbin)
+
