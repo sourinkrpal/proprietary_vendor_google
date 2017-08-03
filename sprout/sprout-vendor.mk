25e7ac115f21d68f3e64587113143a440003e6fd
Samar Vispute <samarvispute121@gmail.com>
2017-08-03 11:00:42 +0530

    bc: -_-

--- sprout/sprout-vendor.mk
+++ sprout/sprout-vendor.mk
@@ -1,14 +1,14 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/app,system/app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/app,system/app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/taido_row/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/xbin,system/xbin)
 
