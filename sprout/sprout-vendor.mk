9b8e834db405bb1c88093c4339a37a8bee971963
Samar Vispute <samarvispute121@gmail.com>
2017-08-05 10:02:02 +0530

    sedlfy

--- sprout/sprout-vendor.mk
+++ sprout/sprout-vendor.mk
@@ -6,8 +6,6 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/propr
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/usr,system/usr)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/vendor,system/vendor)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/google/sprout/proprietary/xbin,system/xbin)
